af37c9b6e4cad31b4b070a6429651aa3526a2fab
AndreiRookie <andrey.idem@gmail.com>
2024-11-26 19:32:13 +0400

    fixed middleIndex

--- src/main/java/org/example/QuickSorter.java
+++ src/main/java/org/example/QuickSorter.java
@@ -10,13 +10,12 @@ public class QuickSorter {
 
             sort(array, pivotIndex + 1, lastIndex);
             sort(array, firstIndex, pivotIndex - 1);
-
         }
     }
 
     private int splitArray(int[] array, int firstIndex, int lastIndex) {
 
-        int middleIndex = array.length / 2;
+        int middleIndex = firstIndex + (lastIndex - firstIndex) / 2;
 
         int pivotValue = array[middleIndex];
         int tempVal = array[middleIndex];
@@ -24,23 +23,23 @@ public class QuickSorter {
         array[middleIndex] = array[lastIndex];
         array[lastIndex] = tempVal;
 
-        int newFirstndex = (firstIndex - 1);
+        int newPivotndex = (firstIndex - 1);
         for (int i = firstIndex; i < lastIndex; i++) {
 
             if (array[i] < pivotValue) {
-                newFirstndex++;
+                newPivotndex++;
 
-                tempVal = array[newFirstndex];
-                array[newFirstndex] = array[i];
+                tempVal = array[newPivotndex];
+                array[newPivotndex] = array[i];
                 array[i] = tempVal;
             }
         }
-        newFirstndex++;
+        newPivotndex++;
 
-        tempVal = array[newFirstndex];
-        array[newFirstndex] = array[lastIndex];
+        tempVal = array[newPivotndex];
+        array[newPivotndex] = array[lastIndex];
         array[lastIndex] = tempVal;
 
-        return newFirstndex;
+        return newPivotndex;
     }
 }
